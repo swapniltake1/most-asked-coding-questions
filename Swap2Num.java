765fd371eaea78b783813338e7eb2f0ddf05b1d0
swapnil take <swapniltake1@outlook.com>
2023-10-15 14:32:20 +0530

    Swapping 2 numbers using 3rd veriable;

---
+++ Swap2Num.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Swap2Num{
+    public static void main(String[] args) {
+        
+        int a, b, temp;
+       // temp=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st value :: ");
+        a=sc.nextInt();
+        System.out.println("Enter the 2st value :: ");
+        b=sc.nextInt();
+
+        System.out.println("Before Swaping a:: "+a + "  & B:: "+b);
+
+        System.out.println("Swapping your numbers ::");
+
+        temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println("value of a::"+a+"  & value of b::"+b);
+        
+
+
+    }
+}
