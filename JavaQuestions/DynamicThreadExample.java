4c099d145c525e71a7122abfe85cd8ece5712720
swapnil take <swapniltake1@outlook.com>
2023-10-17 13:51:59 +0530

    Added Dyanamic thred example

---
+++ JavaQuestions/DynamicThreadExample.java
@@ -0,0 +1,15 @@
+package JavaQuestions;
+
+public class DynamicThreadExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            
+            // Creating threads dynamically using lambda expression
+            Thread thread = new Thread(() -> {
+                System.out.println("Thread " + Thread.currentThread().getId() + " is running.");
+            });
+            thread.start(); // Start the thread
+        }
+    }
+}
+
