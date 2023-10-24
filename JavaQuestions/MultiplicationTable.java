3e3712313fd705ba7aa590b8470f5676dc4bfb74
swapnil take <swapniltake1@outlook.com>
2023-10-24 18:05:04 +0530

    Added MultiplicationTable.java

---
+++ JavaQuestions/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        
+        int a, input;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number for multiplication Table...!!");
+        input=sc.nextInt();
+
+        for(a=1; a<=10; a++){
+                System.out.println("Multiplication of "+input+" * "+a+" = "+input*a);
+        }
+        sc.close();
+    }
+}
