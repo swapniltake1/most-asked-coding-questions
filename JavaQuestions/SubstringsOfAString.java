f445e38a5e44d7924e1477355115f4de199bbd9f
swapnil take <swapniltake1@outlook.com>
2023-10-26 12:44:33 +0530

    SubstringsOfAString.java

---
+++ JavaQuestions/SubstringsOfAString.java
@@ -0,0 +1,32 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+//Find All substring of string in java Program
+public class SubstringsOfAString {
+    public static void main(String[] args) {
+        String string, sub;
+        int i, c,length;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter new String");
+
+        string=sc.nextLine();
+        
+        length=string.length();
+
+        System.out.println("Substring of "+string+" are ::");
+
+        for(c=0; c<length; c++){  // loop for loping code till string ends 
+
+            for( i=0;i<=length+c; i++){    // loop for printing substring
+
+                sub=string.substring(c, c+i);  // create  a sub string
+
+                System.out.println(sub); // printing substring
+
+            }
+        }
+    }
+}
