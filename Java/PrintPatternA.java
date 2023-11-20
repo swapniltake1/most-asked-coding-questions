ed67c5502339321a7db032a350e2019f26a67cfe
swapnil take <swapniltake1@outlook.com>
2023-11-20 18:52:24 +0530

    added print pattern A

---
+++ Java/PrintPatternA.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class PrintPatternA {
+    public static void main(String[] args) {
+        
+        for(int i = 1; i<=10; i++){
+            
+            for(int j=1; j<=i; j++){
+                 System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        
+    }
+}
