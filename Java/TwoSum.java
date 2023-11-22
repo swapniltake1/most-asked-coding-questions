561bff5c722a026e1d28afeef7cd61eaaab2c6c1
swapnil take <swapniltake1@outlook.com>
2023-11-22 17:13:59 +0530

    Added twosum

---
+++ Java/TwoSum.java
@@ -0,0 +1,35 @@
+package Java;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+
+            numMap.put(nums[i], i);
+        }
+
+        // If no solution found
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        if (result != null) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
