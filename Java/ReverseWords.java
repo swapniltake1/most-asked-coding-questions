ac8ce7b7a72f7a3c2fc1e4a0df8391c59e84863c
swapnil take <swapniltake1@outlook.com>
2024-01-18 19:12:23 +0530

    added reverseword.java

--- Java/ReverseWords.java
+++ Java/ReverseWords.java
@@ -3,6 +3,13 @@ package Java;
   Write a Java function that reverses the order of words in a given string.
  Assume that words are separated by a single space and that there are no leading or trailing spaces. 
  The function should not use any built-in library functions for reversing a string.
+
+ Input: "Hello World"
+Output: "World Hello"
+
+Input: "Practice Coding Questions"
+Output: "Questions Coding Practice"
+
  
 */
 public class ReverseWords {
